6d490ee8b9de095831f87e91e7c727f5c0cb53af
chenlianfu <chenllianfu@foxmail.com>
2022-04-17 17:01:54 +0800

    submit20220417

--- bin/blast.pl
+++ bin/blast.pl
@@ -1,51 +1,162 @@
 #!/usr/bin/perl
 use strict;
+use Getopt::Long;
 
 my $usage = <<USAGE;
 Usage:
-    perl homolog_paraBlast blastProgramType blastDB fastaFile evalue threads outPrefix outfmt
-    7 parameters should be given, and the final result is outPrefix.xml or outPrefix.tab
+    $0 [options] BLAST_DB file.fasta > out.txt
 
-For example:
-    perl homolog_paraBlast blastp nr proteins.fa 1e-3 24 nr 5
+    --tmp-prefix <string>    default: blast
+    设置临时文件或文件夹前缀。默认设置下，程序生成command.blast.list，blast.tmp/等临时文件或目录。
+
+    --chunk <int>    default: 10
+    设置每个数据块的序列条数。程序会将输入FASTA文件中的序列从前往后分割成多份，每10条相邻的序列分配到一个FASTA文件中；在blast.tmp/临时文件夹下生成次级文件夹，每个文件夹做多放置10个FASTA文件；每个fasta文件写出一条BLAST命令到command.blast.list文件中；然后程序调用ParaFly进行并行化计算。
+    请注意：若数据块的数量超过100万个，默认设置下blast.tmp/文件夹中的目录数量太多（超过1万个），导致文件系统运行缓慢，ParaFly程序运行效率低下，无法充分利用服务器计算资源。此时推荐设置--chunk参数值为100。
+
+    --blast-program <string>    default: blastp
+    设置运行的BLAST命令，支持的命令有：blastn, blastp, blastx, tblastn, tblastx。
+
+    --CPU <int>    default: 1
+    设置并行运行的BLAST程序个数。
+
+    --blast-threads <int>    default: 1
+    设置BLAST命令的-num_threads参数值。该参数让每个BLAST命令可以多线程运行。
+    请注意：--blast-threads参数值和--CPU参数值的乘积不要超过服务器的CPU总计算线程数。
+
+    --evalue <float>    default: 1e-3
+    设置BLAST命令的-evalue参数值。
+
+    --outfmt <int>    default: 5
+    设置BLAST命令的-outfmt参数值。输出方式。若为5，则输出xml格式结果，若为6或7，则输出表格结果。
+
+    --max-target-seqs <int>    default: 20
+    设置BLAST命令的-max_target_seqs参数值。该参数设置BLAST最多能匹配数据库中的序列数量。
+
+    --clean
+    若添加该参数，则在运行程序成功后，会删除临时文件或文件夹。
 
 USAGE
-if (@ARGV != 7){die $usage}
+if (@ARGV==0){die $usage}
 
-my ($program, $db, $fasta, $evalue, $threads, $outPrefix, $outfmt) = @ARGV;
-open IN, $fasta or die $!;
-my ($seqID, @seqID, %seq);
+my ($tmpPrefix, $chunk, $blastProgram, $CPU, $blastThreads, $evalue, $outfmt, $maxTargetSeqs, $clean);
+GetOptions(
+    "tmp-prefix:s" => \$tmpPrefix,
+    "chunk:i" => \$chunk,
+    "blast-program:s" => \$blastProgram,
+    "CPU:i" => \$CPU,
+    "blast-threads:i" => \$blastThreads,
+    "evalue:f" => \$evalue,
+    "outfmt:i" => \$outfmt,
+    "max-target-seqs:i" => \$maxTargetSeqs,
+    "clean!" => \$clean,
+);
+$tmpPrefix ||= "blast";
+$chunk ||= 10;
+$blastProgram ||= "blastp";
+$CPU ||= 1;
+$blastThreads ||= 1;
+$evalue ||= 1e-3;
+$outfmt ||= 5;
+$maxTargetSeqs ||= 20;
+
+my %blastProgram = ("blastn", 1, "blastp", 1, "blastx", 1, "tblastn", 1, "tblastx", 1);
+if (! exists $blastProgram{$blastProgram}) {
+    die "$blastProgram was not a supportted BLAST command (blastn, blastp, blastx, tblastn, tblastx)!\n";
+}
+
+mkdir "$tmpPrefix.tmp" unless -e "$tmpPrefix.tmp";
+open IN, $ARGV[1] or die "Can not open file $ARGV[1], $!\n";
+open CMD, ">", "command.$tmpPrefix.list" or die "Cannot create file command.$tmpPrefix.list, $!\n";
+
+my ($fasta, @chunk);
+my ($number_L1, $number_L2, $chunk_L1_number, $chunk_L2_number) = (1, 0, 1, 0);
+my $chunk_L1_name = "chunk" . '0' x (5 - length($chunk_L1_number)) . $chunk_L1_number;
+mkdir "$tmpPrefix.tmp/$chunk_L1_name" unless -e "$tmpPrefix.tmp/$chunk_L1_name";
 while (<IN>) {
-    chomp;
-    if (/^>(\S+)/) { $seqID = $1; push @seqID, $seqID; }
-    else           { $seq{$seqID} .= $_ }
-}
+    if (m/^>/) {
+        $number_L2 ++;
 
-mkdir "$outPrefix.tmp" unless -e "$outPrefix.tmp";
-open COM, '>', "$outPrefix.command" or die $!;
+        if ($number_L2 > $chunk) {
+            $chunk_L2_number ++;
+            my $chunk_L2_name = '0' x (length($chunk) - length($chunk_L2_number)) . $chunk_L2_number;
+            push @chunk, "$tmpPrefix.tmp/$chunk_L1_name/$chunk_L2_name";
+            open OUT, ">", "$tmpPrefix.tmp/$chunk_L1_name/$chunk_L2_name.fasta" or die "Can not create file $tmpPrefix.tmp/$chunk_L1_name/$chunk_L2_name.fasta, $!\n";
+            print OUT $fasta;
+            print CMD "$blastProgram -query $tmpPrefix.tmp/$chunk_L1_name/$chunk_L2_name.fasta -db $ARGV[0] -num_threads $blastThreads -evalue $evalue -outfmt $outfmt -max_target_seqs $maxTargetSeqs -out $tmpPrefix.tmp/$chunk_L1_name/$chunk_L2_name.out\n";
+            close OUT;
+            $number_L2 = 1;
+            $number_L1 ++;
+            $fasta = "";
+        }
 
-foreach my $seq_id (@seqID) {
-    $_ = $seq_id;
-    s/\|/\\\|/;
-    open FASTA, '>', "$outPrefix.tmp/$seq_id.fa" or die $!;
-    print FASTA ">$seq_id\n$seq{$seq_id}\n";
-    close FASTA;
-
-    print COM "$program -query $outPrefix.tmp/$_.fa -db $db -evalue $evalue -num_threads 1 -outfmt $outfmt -out $outPrefix.tmp/$_.out\n";
-}
-
-system `ParaFly -c $outPrefix.command -CPU $threads`;
-
-unless ( -e "FailedCommands" ) {
-    if ($outfmt == 5) { open OUT, '>', "$outPrefix.xml" or die $! }
-    else              { open OUT, '>', "$outPrefix.tab" or die $! }
-
-    foreach (@seqID) {
-        open IN, "$outPrefix.tmp/$_.out" or die $!;
-        print OUT <IN>;
+        if ($number_L1 > $chunk) {
+            $chunk_L1_number ++;
+            $chunk_L1_name = "chunk" . '0' x (5 - length($chunk_L1_number)) . $chunk_L1_number;
+            mkdir "$tmpPrefix.tmp/$chunk_L1_name" unless -e "$tmpPrefix.tmp/$chunk_L1_name";
+            $number_L1 = 1;
+            $chunk_L2_number = 0;
+        }
     }
+    $fasta .= $_;
+}
+if ($fasta) {
+    $chunk_L2_number ++;
+    my $chunk_L2_name = '0' x (length($chunk) - length($chunk_L2_number)) . $chunk_L2_number;
+    push @chunk, "$tmpPrefix.tmp/$chunk_L1_name/$chunk_L2_name";
+    open OUT, ">", "$tmpPrefix.tmp/$chunk_L1_name/$chunk_L2_name.fasta" or die "Can not create file $tmpPrefix.tmp/$chunk_L1_name/$chunk_L2_name.fasta, $!\n";
+    print OUT $fasta;
+    print CMD "$blastProgram -query $tmpPrefix.tmp/$chunk_L1_name/$chunk_L2_name.fasta -db $ARGV[0] -num_threads $blastThreads -evalue $evalue -outfmt $outfmt -max_target_seqs $maxTargetSeqs -out $tmpPrefix.tmp/$chunk_L1_name/$chunk_L2_name.out\n";
+    close OUT;
+}
+close CMD;
 
-    system `rm -rf $outPrefix.tmp`;
-    unlink "$outPrefix.command";
-    unlink "$outPrefix.command.completed";
+my $cmdString = "ParaFly -c command.$tmpPrefix.list -CPU $CPU &> /dev/null";
+print STDERR "CMD: $cmdString\n";
+(system $cmdString) == 0 or die "Failed to execute: $cmdString\n";
+
+foreach (@chunk) {
+    if ($outfmt == 5) {
+        open IN, "$_.out" or die "Can not open file $_.out, $!\n";
+        my $start .= <IN>;
+        foreach (1 .. 19) {
+            $start .= <IN>;
+        }
+        my @lines = <IN>;
+        my $end = pop @lines;
+        $end = (pop @lines) . $end;
+        $end = (pop @lines) . $end;
+        #print "$start$end";
+
+        my (@out, $one_interation);
+        foreach (@lines) {
+            $one_interation .= $_;
+            if (m/^<\/Iteration>/) {
+                $one_interation =~ s#<Iteration_iter-num>\d+</Iteration_iter-num>#<Iteration_iter-num>1</Iteration_iter-num>#;
+                $one_interation =~ s#<Iteration_query-ID>.*?</Iteration_query-ID>#<Iteration_query-ID>Query_1</Iteration_query-ID>#;
+                my $query_def = $1 if $one_interation =~ m#<Iteration_query-def>(.*?)</Iteration_query-def>#;
+                my $query_len = $1 if $one_interation =~ m#<Iteration_query-len>(\d+)</Iteration_query-len>#;
+                my $start_new = $start;
+                $start_new =~ s#<BlastOutput_query-def>.*?</BlastOutput_query-def>#<BlastOutput_query-def>$query_def</BlastOutput_query-def>#;
+                $start_new =~ s#<BlastOutput_query-len>\d+</BlastOutput_query-len>#<BlastOutput_query-len>$query_len</BlastOutput_query-len>#;
+                push @out, "$start_new$one_interation$end";
+                $one_interation = "";
+            }
+        }
+
+        foreach (@out) {
+            print;
+        }
+        close IN;
+    }
+    else {
+        open IN, "$_.out" or die "Can not open file $_.out, $!\n";
+        print <IN>;
+        close IN;
+    }
+}
+
+if ($clean) {
+    unlink "command.$tmpPrefix.list";
+    unlink "command.$tmpPrefix.list.completed";
+    system `rm -rf $tmpPrefix.tmp`;
 }
