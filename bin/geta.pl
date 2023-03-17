24db5cca867870aedf0efcc5b5a4bf9b4f63dbaa
chenlianfu <chenlianfu@foxmail.com>
2023-03-17 22:15:23 +0800

    submit 20230317

--- bin/geta.pl
+++ bin/geta.pl
@@ -330,12 +330,14 @@ unless (-e "0.RepeatMasker.ok") {
         else {
             print STDERR "CMD(Skipped): $cmdString\n";
         }
-        my $cpu_RepeatModeler = int($cpu / 4);
+        my $cpu_RepeatModeler = $cpu;
         $cmdString = "RepeatModeler -threads $cpu_RepeatModeler -database species -LTRStruct &> RepeatModeler.log";
         # 若RepeatModeler版本为2.0.3或更低，则需要将-threads参数换为-pa参数。
         my $RepeatModeler_info = `RepeatModeler`;
         my $RepeatModeler_version = $1 if $RepeatModeler_info =~ m/RepeatModeler - ([\d\.]+)/;
         if ( $RepeatModeler_version =~ m/(\d+)\.(\d+)\.(\d+)/ && ($1 < 2 or ( $1 == 2 && $1 == 0 && $3 <= 3)) )  {
+			$cpu_RepeatModeler = int($cpu / 4);
+			$cpu_RepeatModeler = 1 if $cpu_RepeatModeler < 1;
             $cmdString = "RepeatModeler -pa $cpu_RepeatModeler -database species -LTRStruct &> RepeatModeler.log";
         }
         unless (-e "RepeatModeler.ok") {
